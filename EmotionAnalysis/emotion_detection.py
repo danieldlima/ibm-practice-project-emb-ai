37e4ede695558060b96e31092ce5200b927fc8d8
Daniel Lima <familiasilva.daniel@gmail.com>
2024-01-19 16:07:01 -0300

    refactor(packages): refactor emotion detection and improve error handling
    This update involves a significant refactoring of the emotion detection mechanism to provide more comprehensive emotional insights. It includes altering the API endpoints and headers to improve emotion prediction. Additionally, it introduces robust error handling to account for potential request exceptions.

--- EmotionAnalysis/emotion_detection.py
+++ EmotionAnalysis/emotion_detection.py
@@ -1,23 +1,45 @@
 """ Module providing functionality for working with sentiment using IBM Watson API
 """
 import json
+
 import requests
+
+BASE_URL = ('https://sn-watson-emotion.labs.skills.network/v1'
+            '/watson.runtime.nlp.v1/NlpService/EmotionPredict')
+HEADERS = {'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'}
 
 def emotion_detector(text_analyse):
     """ Function to perform sentiment analysis based on text provided as a parameter
     """
-    url = ('https://sn-watson-emotion.labs.skills.network/v1'
-           '/watson.runtime.nlp.v1/NlpService/EmotionPredict')
-    header = {'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'}
     data = {'raw_document': { 'text': text_analyse }}
+    try:
+        raw_response = requests.post(BASE_URL, headers=HEADERS, json=data, timeout=10)
+        response = json.loads(raw_response.text)
+        return _process_response(response)
+    except requests.exceptions.RequestException as error:
+        print(f"[error]: {error}")
+        return None
 
-    response = requests.post(url, headers=header, json=data, timeout=10)
-    formatted_response = json.loads(response.text)
+def _process_response(response):
+    emotion_predictions = response.get('emotionPredictions', {})
+    if not emotion_predictions:
+        return None
 
-    label = None
-    score = None
-    if response.status_code == 200:
-        label = formatted_response.get('documentSentiment', {}).get('label')
-        score = formatted_response.get('documentSentiment', {}).get('score')
+    return _get_emotions_and_dominant(emotion_predictions)
 
-    return {'label': label, 'score': score}
+def _get_emotions_and_dominant(emotion_predictions):
+    first_prediction = emotion_predictions[0].get('emotion')
+    if not first_prediction:
+        return None
+
+    emotions = {k: first_prediction.get(k) for k in ['anger', 'disgust', 'fear', 'joy', 'sadness']}
+    dominant_emotion = _get_dominant(emotions)
+    emotions.update({'dominant_emotion': dominant_emotion})
+
+    return emotions
+
+def _get_dominant(emotions):
+    max_emotion_value = max(emotions.values())
+    for emotion, value in emotions.items():
+        if value == max_emotion_value:
+            return emotion.upper()
