157d3c36a6c5bc89f53e9502d117f4bdcbbbb156
Daniel Lima <familiasilva.daniel@gmail.com>
2024-01-19 17:25:51 -0300

    test(tests): expand emotion detection test cases
    The existing sentiment analysis tests have been refactored to include wider range of emotional states: anger, disgust, fear, joy, and sadness. This comprehensive test coverage will ensure more accurate and precise results from the emotion detection system.

--- tests/test_emotion_detection.py
+++ tests/test_emotion_detection.py
@@ -2,15 +2,29 @@ import unittest
 
 from EmotionDetection.emotion_detection import emotion_detector
 
-class TestEmotionAnalysis(unittest.TestCase):
-    def test_sentiment_analyzer(self): # I love this new technology
-        sentiment_positive = emotion_detector("I love working with Python")
-        sentiment_negative = emotion_detector("I hate working with Python")
-        sentiment_neutral = emotion_detector("I am neutral on Python")
+EMOTIONS = {
+    'anger': {"emotion": 'ANGER', "text": "I am really mad about this"},
+    'disgust': {"emotion": 'DISGUST', "text": "I feel disgusted just hearing about this"},
+    'fear': {"emotion": 'FEAR', "text": "I am really afraid that this will happen"},
+    'joy': {"emotion": 'JOY', "text": "I am glad this happened"},
+    'sadness': {"emotion": 'SADNESS', "text": "I am so sad about this"}
+}
 
-        self.assertEqual(sentiment_positive['label'], 'SENT_POSITIVE')
-        self.assertEqual(sentiment_negative['label'], 'SENT_NEGATIVE')
-        self.assertEqual(sentiment_neutral['label'], 'SENT_NEUTRAL')
+class TestEmotionDetection(unittest.TestCase):
+    def test_sentiment_analyzer(self):
+        anger = emotion_detector(EMOTIONS['anger']['text'])
+        disgust = emotion_detector(EMOTIONS['disgust']['text'])
+        fear = emotion_detector(EMOTIONS['fear']['text'])
+        joy = emotion_detector(EMOTIONS['joy']['text'])
+        sadness = emotion_detector(EMOTIONS['sadness']['text'])
+        is_none = emotion_detector("")
+
+        self.assertEqual(anger['dominant_emotion'], EMOTIONS['anger']['emotion'])
+        self.assertEqual(disgust['dominant_emotion'], EMOTIONS['disgust']['emotion'])
+        self.assertEqual(fear['dominant_emotion'], EMOTIONS['fear']['emotion'])
+        self.assertEqual(joy['dominant_emotion'], EMOTIONS['joy']['emotion'])
+        self.assertEqual(sadness['dominant_emotion'], EMOTIONS['sadness']['emotion'])
+        self.assertIsNone(is_none)
 
 if __name__ == '__main__':
     unittest.main()
